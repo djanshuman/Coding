c3e433ffb3d983a3a60eb7efa69c349d016291d1
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2023-08-28 18:48:47 -0700

    Update PalindromicSubstrings.py

--- Leetcode-Blind-75/PalindromicSubstrings.py
+++ Leetcode-Blind-75/PalindromicSubstrings.py
@@ -20,3 +20,28 @@ class Solution:
 
 my_obj = Solution()
 print(my_obj.countSubstrings("aaa"))
+
+'''
+
+class Solution:
+    def lengthLongestSubstring(self,s:str)-> int:
+        count=0
+
+        for i in range(len(s)):
+            count=self.checkPalin(s,i,i+1,count)
+            count=self.checkPalin(s,i,i,count)
+
+        return count
+
+    def checkPalin(self,s,l,r,count):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+                count+=1
+                l-=1
+                r+=1
+        return count
+
+myObj= Solution()
+s = "aaa"
+print(myObj.lengthLongestSubstring(s))
+
+'''
