8cdcfd2d5144799ac49393ae78cedc7317e9d40a
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2023-08-28 18:35:16 -0700

    Update LongestPalindromicSubstring.py

--- Leetcode-Blind-75/LongestPalindromicSubstring.py
+++ Leetcode-Blind-75/LongestPalindromicSubstring.py
@@ -24,3 +24,33 @@ class Solution:
 
 my_obj = Solution()
 print(my_obj.longestPalindrome("cbbd"))
+
+
+'''
+
+
+class Solution:
+    def lengthLongestSubstring(self,strs:str)-> str:
+        res=""
+        resLen=0
+
+        for i in range(len(strs)):
+            res,resLen=self.checkPalin(strs,i,i+1,resLen,res)
+            res,resLen =self.checkPalin(strs,i,i,resLen,res)
+
+        return res
+
+    def checkPalin(self,strs,l,r,resLen,res):
+        while l >= 0 and r < len(s) and strs[l] == strs[r]:
+            if (r-l+1) > resLen:
+                res=strs[l:r+1]
+                resLen= r-l+1
+            l-=1
+            r+=1
+        return res,resLen
+
+myObj= Solution()
+s = "ac"
+print(myObj.lengthLongestSubstring(s))
+
+'''
